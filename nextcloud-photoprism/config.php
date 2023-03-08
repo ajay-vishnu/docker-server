f29a0a744352210c48c38b43071359958a133710
Rohit <rohit.yentruvi.794@gmail.com>
2023-03-08 22:31:05 +0500

    Update: created compose files for the server

---
+++ nextcloud-photoprism/config.php
@@ -0,0 +1,41 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocnrw7spsbgu',
+  'passwordsalt' => 'b8bsaBFu6iAJYVeu83FEtwN2FW1iSI',
+  'secret' => 'JcaQyi0yuhWRPBAxI/DB8zFgUeAzgsj+cycOdgYZCS4iP62D',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.11:8122',
+    1 => 'curly-sheep-31.telebit.io',
+    2 => '8b35-122-50-209-92.in.ngrok.io',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '25.0.4.1',
+  'overwrite.cli.url' => 'http://192.168.1.11:8122',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'hublibhadya',
+  'installed' => true,
+);
